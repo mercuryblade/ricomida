358bc9489aa7205b00003d73844c56ae0ea0a6ab
felipe <felipevillegas08@hotmail.com>
2024-08-21 21:48:01 -0400

    se agrega función enviar a correo

---
+++ assets/JS/logico.js
@@ -0,0 +1,11 @@
+// Inicializar tooltips
+document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+})
+
+function enviar_correo() {
+    alert("El correo fue enviado correctamente");
+}
